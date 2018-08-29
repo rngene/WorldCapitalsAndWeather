eaa29f3cf098f8b6b6d1a383ac4ba5ad9737adbb
ramong <gramon@carnival.com>
2018-08-29 10:31:45 -0400

    Test 5

--- WorldCapitalsAndWeather.Test/TestStartup.cs
+++ WorldCapitalsAndWeather.Test/TestStartup.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System;
+using System.IO;
+using System.Net;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
+using RichardSzalay.MockHttp;
 using WorldCapitalsAndWeather.Services;
 
 namespace WorldCapitalsAndWeather.Test
 {
     public class TestStartup:Startup
     {
+        private static readonly Lazy<string> _weatherContent = new Lazy<string>(() => File.ReadAllText(@"Data\Weather.json"));
         public TestStartup(IConfiguration configuration) : base(configuration)
         {
         }
@@ -15,10 +20,17 @@ namespace WorldCapitalsAndWeather.Test
         {
             ConfigureServices(services);
 
-            var weatherServiceMock = new Mock<IWeatherService>();
-            weatherServiceMock.Setup(w => w.GetWeather(It.IsAny<string>())).ReturnsAsync("87.0");
+            //var weatherServiceMock = new Mock<IWeatherService>();
+            //weatherServiceMock.Setup(w => w.GetWeather(It.IsAny<string>())).ReturnsAsync("87.0");
 
-            services.AddSingleton(weatherServiceMock.Object);
+            //services.AddSingleton(weatherServiceMock.Object);
+
+            var mockHttp = new MockHttpMessageHandler();
+
+            mockHttp.When("http://dataservice.accuweather.com/currentconditions/v1/*")
+                .Respond(HttpStatusCode.OK, "application/json", _weatherContent.Value);
+
+            services.AddSingleton(mockHttp.ToHttpClient());
         }
     }
 }
--- WorldCapitalsAndWeather/Services/WeatherService.cs
+++ WorldCapitalsAndWeather/Services/WeatherService.cs
@@ -1,7 +1,9 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using WorldCapitalsAndWeather.Models;
 
